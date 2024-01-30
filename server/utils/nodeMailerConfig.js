dcd4aa07d4776e838138249350e436dc0b8d92e6
Faraz <104452510+GeekkyCoder@users.noreply.github.com>
2024-01-30 21:30:14 +0500

    Update nodeMailerConfig.js

--- server/utils/nodeMailerConfig.js
+++ server/utils/nodeMailerConfig.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  pool: true,
-  service: process.env.SERVICE,
+  service: "gmail",
   secure: process.env.NODE_ENV === 'production',
-  port: process.env.PORT,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
